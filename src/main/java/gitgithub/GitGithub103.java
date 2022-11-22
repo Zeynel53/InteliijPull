67f239aa4b7f6d662488a82b1cf34c1038e80a9f
zeynel53 <acikmesezeynel@gmail.com>
2022-11-22 10:21:11 +0300

    V2

--- src/main/java/gitgithub/GitGithub103.java
+++ src/main/java/gitgithub/GitGithub103.java
@@ -3,5 +3,7 @@ package gitgithub;
 public class GitGithub103 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("GitGithub ilk deneme");
+        System.out.println();
     }
 }
